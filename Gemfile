83dc202fb44f05762fc89d7801972f338586682c
Charles Randria <oh.randria@gmail.com>
2017-02-16 11:13:37 +0100

    rails new

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
+ruby '2.3.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
 # Use sqlite3 as the database for Active Record
@@ -52,3 +52,9 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rspec-rails', group: [ :test ]
+
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'pg'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,41 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+
+  get 'doses/create'
+
+  get 'doses/edit'
+
+  get 'doses/update'
+
+  get 'doses/destroy'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
+  get 'cocktails/create'
+
+  get 'cocktails/edit'
+
+  get 'cocktails/update'
+
+  get 'cocktails/destroy'
+
+  get 'ingredients/index'
+
+  get 'ingredients/show'
+
+  get 'ingredients/new'
+
+  get 'ingredients/create'
+
+  get 'ingredients/edit'
+
+  get 'ingredients/update'
+
+  get 'ingredients/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
